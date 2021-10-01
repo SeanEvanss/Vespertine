c5cc6a34ea3c92a87d7d286e1ae90c5dbde7932c
Sean Tang <1996sean@gmail.com>
2021-10-01 20:53:41 +0800

    Fixed API key build config

--- app/src/main/java/com/example/firstapplication/BusActivity.java
+++ app/src/main/java/com/example/firstapplication/BusActivity.java
@@ -12,7 +12,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
